3eb012819711611f642485e4ea023b9d43dc04ad
Aayush Pathak <118837534+ayushpathak-48@users.noreply.github.com>
2023-06-04 22:52:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener("keydown", (e) => {
+  document.getElementById(
+    "result"
+  ).innerHTML = `The key pressed is <span>${e.key}</span><span>Key Code: ${e.keyCode}</span>`;
+});
